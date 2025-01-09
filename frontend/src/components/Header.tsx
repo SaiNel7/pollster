8cb8a65da9336c407301dfdae6c0bc2881ba7549
Nicholas Channg <87846996+NicholasChanng@users.noreply.github.com>
2025-01-08 19:28:03 -0500

    initial home page

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import "../assets/css/Header.css";
+
+function Header() {
+  return (
+    <section className="header-container">
+      <h1>poll/str</h1>
+      <div className="user-login">
+        <p className="styled-button">Sign In</p>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/css/Home.css";
+function Home() {
+  return (
+    <>
+      <section>
+        <div className="home-hero">
+          <h1>for all the times you wanted to know</h1>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
